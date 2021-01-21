24cdfb09149559d59f68ad9f8575bf78b4846b2f
Sidharth Praveen <sidhu.beena@gmail.com>
2021-01-21 22:04:53 +0800

    Update server.py

--- flask/server.py
+++ flask/server.py
@@ -18,59 +18,6 @@ app = Flask(__name__)
 model = Model()
 chunk_duration = 300 #300 slices per chunk. user input must be 300, generated will be 300
 bpm = 120
-# @app.route('/generate_from_user_input', methods=['GET', 'POST'])
-# @cross_origin()
-# def generate_from_user_input():    
-#     """
-#     should be array of dictionarys of user input slices
-#     """
-
-#     combiSequence = request.json['recorded_result']
-#     if combiSequence == None:
-#         return "invalid input"
-#     if len(combiSequence) > chunk_duration:
-#         combiSequence = combiSequence[:chunk_duration]
-    
-#     if len(combiSequence) < chunk_duration:
-#         #pad sequences
-#         print(f"sequence has length {len(combiSequence)}. adding padding...")
-#         while len(combiSequence) < chunk_duration:
-#             combiSequence.insert(0, [])
-    
-#     #convert 2d list into (list, tuple)
-#     combiSequence = [tuple(combi) for combi in combiSequence]
-    
-#     # song_piano_roll = np.load('./encoded_SmallDay.npy').astype(np.int8)
-#     # input_piano_roll = song_piano_roll[:chunk_duration]    
-#     midiBytesIO = model.generateMidiBytesFromCombiSequence(combiSequence, chunk_duration)
-#     midiBytesIO.seek(0)
-#     return flask.send_file(midiBytesIO, as_attachment=True, attachment_filename='generated.mid')
-
-# @app.route('/user_input_to_midi', methods=['GET', 'POST'])
-# @cross_origin()
-# def user_input_to_midi():    
-#     """
-#     should be array of dictionarys of user input slices
-#     """
-
-#     combiSequence = request.json['recorded_result']
-#     if combiSequence == None:
-#         return "invalid input"
-#     if len(combiSequence) > chunk_duration:
-#         combiSequence = combiSequence[:chunk_duration]
-    
-#     if len(combiSequence) < chunk_duration:
-#         #pad sequences
-#         print(f"sequence has length {len(combiSequence)}. adding padding...")
-#         while len(combiSequence) < chunk_duration:
-#             combiSequence.insert(0, [])
-    
-#     #convert 2d list into (list, tuple)
-#     combiSequence = [tuple(combi) for combi in combiSequence]
-#     midiBytesIO = model.combiSequenceToMidiBytes(combiSequence)
-#     midiBytesIO.seek(0)
-#     return flask.send_file(midiBytesIO, as_attachment=True, attachment_filename='generated.mid')
-
 
 @app.route('/user_combi_to_token_sequence', methods=['POST'])
 @cross_origin()
